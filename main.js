efaa49b7dcf911a4355b8f41afe721721a43f733
Elisha Adeboye <y.adeboye@gmail.com>
2023-06-23 14:33:24 +0100

    added graphic design tab

--- main.js
+++ main.js
@@ -68,5 +68,52 @@
            resizeFix();
        }
     });
-  
+
+    
+            // Filter switch start
+
+            // Select relevant HTML elements
+            const filterButtons = document.querySelectorAll("#filter-buttons button");
+            const filterableCards = document.querySelectorAll("#filterable-cards .project");
+    
+            // Function to filter cards based on filter buttons
+            const filterCards = (e) => {
+                document.querySelector("#filter-buttons .active").classList.remove("active");
+                e.target.classList.add("active");
+    
+                filterableCards.forEach(project => {
+                    // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+                    if(project.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+                        return project.classList.replace("hide", "show");
+                    }
+                    project.classList.add("hide");
+                });
+            }
+    
+            filterButtons.forEach(button => button.addEventListener("click", filterCards));
+      // Filter switch end
+
+
+    const allImages = document.querySelectorAll(".images .img");
+    const lightbox = document.querySelector(".lightbox");
+    const closeImgBtn = lightbox.querySelector(".close-icon");
+
+    allImages.forEach(img => {
+    // Calling showLightBox function with passing clicked img src as argument
+    img.addEventListener("click", () => showLightbox(img.querySelector("img").src));
+    });
+
+    const showLightbox = (img) => {
+    // Showing lightbox and updating img source
+    lightbox.querySelector("img").src = img;
+    lightbox.classList.add("show");
+    document.body.style.overflow = "hidden";
+    }
+
+    closeImgBtn.addEventListener("click", () => {
+    // Hiding lightbox on close icon click
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+    });
+
   })();
